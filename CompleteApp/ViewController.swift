04a2b39e63dffed149cd9c677501a0a85447207d
Мусаев94 <beksultan1923@gmail.com>
2021-12-27 19:42:20 +0600

    Initial Commit

---
+++ CompleteApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompleteApp
+//
+//  Created by user on 27/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
